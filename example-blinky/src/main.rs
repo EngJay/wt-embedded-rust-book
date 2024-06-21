994d74d3b6b0de67e8a6d41360a1392de00986f6
Jason Scott <jpscott07@gmail.com>
2024-06-21 15:07:39 -0400

    fix linker errors by including modules that provide the expected vector table

--- example-blinky/src/main.rs
+++ example-blinky/src/main.rs
@@ -1,14 +1,23 @@
+#![deny(unsafe_code)]
 #![no_std]
 #![no_main]
 
 // pick a panicking behavior
 use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
+
 // use panic_abort as _; // requires nightly
 // use panic_itm as _; // logs messages over ITM; requires ITM support
 // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
 use cortex_m::asm;
 use cortex_m_rt::entry;
+
+use stm32f3_discovery::stm32f3xx_hal::delay::Delay;
+use stm32f3_discovery::stm32f3xx_hal::pac;
+use stm32f3_discovery::stm32f3xx_hal::prelude::*;
+
+use stm32f3_discovery::leds::Leds;
+use stm32f3_discovery::switch_hal::{OutputSwitch, ToggleableOutputSwitch};
 
 #[entry]
 fn main() -> ! {
