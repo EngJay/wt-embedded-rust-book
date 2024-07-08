50814684fd9a35265a0abbdb7e3e1f9fe7ef1964
Jason Scott <jpscott07@gmail.com>
2024-07-08 13:43:20 -0400

    configure for defmt and probe-rs, runs on target now

--- examples/i2c/stm32f3-disco/src/main.rs
+++ examples/i2c/stm32f3-disco/src/main.rs
@@ -1,16 +1,48 @@
 #![no_std]
 #![no_main]
 
+use cortex_m_semihosting::debug;
+
+use defmt_rtt as _; // global logger
+
 // pick a panicking behavior
-use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-                     // use panic_abort as _; // requires nightly
-                     // use panic_itm as _; // logs messages over ITM; requires ITM support
-                     // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+// use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
+// use panic_abort as _; // requires nightly
+// use panic_itm as _; // logs messages over ITM; requires ITM support
+// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+use panic_probe as _;
 
 use cortex_m::asm;
 use cortex_m_rt::entry;
 
 use stm32f3xx_hal::{delay::Delay, pac, prelude::*};
+
+// same panicking *behavior* as `panic-probe` but doesn't print a panic message
+// this prevents the panic message being printed *twice* when `defmt::panic` is invoked
+#[defmt::panic_handler]
+fn panic() -> ! {
+    cortex_m::asm::udf()
+}
+
+/// Terminates the application and makes a semihosting-capable debug tool exit
+/// with status code 0.
+pub fn exit() -> ! {
+    loop {
+        debug::exit(debug::EXIT_SUCCESS);
+    }
+}
+
+/// Hardfault handler.
+///
+/// Terminates the application and makes a semihosting-capable debug tool exit
+/// with an error. This seems better than the default, which is to spin in a
+/// loop.
+#[cortex_m_rt::exception]
+unsafe fn HardFault(_frame: &cortex_m_rt::ExceptionFrame) -> ! {
+    loop {
+        debug::exit(debug::EXIT_FAILURE);
+    }
+}
 
 #[entry]
 fn main() -> ! {
